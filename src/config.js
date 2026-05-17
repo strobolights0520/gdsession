8f5c7d4a613321974a057924ef858822949c1ee4
strobolights0520 <info@strobolights.net>
2026-05-17 16:08:00 +0900

    Delete src/config.js

--- src/config.js
+++
@@ -1,5 +0,0 @@
-window.OPEN_GD_CONFIG = {
-  SUPABASE_URL: "https://ssavwsfqzrkvvvekloaj.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_xYT51KUOTcBLvOoJh_kTRA_3aCZUyzn",
-  ADMIN_EMAILS: [],
-};
